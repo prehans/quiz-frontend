332bb72020391c745eed7d1da2e5d98db3da0662
prehans <prehansgupta@gmail.com>
2023-10-01 02:08:42 +0530

    first commit

---
+++ src/pages/Homepage.js
@@ -0,0 +1,110 @@
+import { React, useEffect } from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Center,
+  Stack,
+  Link,
+} from "@chakra-ui/react";
+import Login from "../components/authentication/Login";
+import Signup from "../components/authentication/Signup";
+import { useNavigate } from "react-router-dom";
+
+const Homepage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+    if (user) {
+      navigate("/main");
+    }
+  }, [navigate]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="1g"
+        borderWidth="1px"
+        boxShadow="0 0 0 3px black"
+          
+        
+      >
+        <Text fontSize={{ base: "3xl", md: "4xl" }} fontFamily="Work sans">
+          <Center>Quiz Hub</Center>
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px" boxShadow="0 0 0 3px black">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+      <Container
+        as={Stack}
+        maxW={"6xl"}
+        mt={{ base: "10px", md: "50px" }}
+        py={4}
+        direction={{ base: "column", md: "row" }}
+        spacing={4}
+        justify={{ base: "center", md: "space-between" }}
+        align={{ base: "center", md: "center" }}
+      >
+        <Stack
+          color="black"
+          fontFamily="Work sans"
+          fontWeight="bold"
+          direction={"row"}
+          spacing={6}
+        >
+          <Link href={"/"} target="_blank">
+            GitHub
+          </Link>
+          <Link
+            href={"/"}
+            target="_blank"
+          >
+            LinkedIn
+          </Link>
+          <Link
+            href={"/"}
+            target="_blank"
+          >
+            FaceBook
+          </Link>
+        </Stack>
+        <Text
+          color="black"
+          fontFamily="Work sans"
+          fontWeight="bold"
+          fontSize="16px"
+        >
+          Made with ❤️ by Prehans Gupta
+        </Text>
+      </Container>
+    </Container>
+  );
+};
+
+export default Homepage;
---
+++ src/pages/Mainpage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import Navbar from "../components/others/navbar";
+
+const Mainpage = () => {
+  return (
+    <>
+      <Navbar />
+      <Box
+        minH="100vh"
+        display="flex"
+        justifyContent="center"
+        textAlign="center"
+        flexDirection="column"
+        bgGradient="linear(to-b, white, white)"
+        mx={{ base: "0", md: "auto" }}
+        p={4}
+      >
+        <Heading
+          as="h1"
+          fontSize={{ base: "4xl", md: "5xl" }}
+          color="black"
+          mb={4}
+        >
+          Welcome to the Language Learning Quiz App!
+        </Heading>
+        <Text fontSize={{ base: "xl", md: "2xl" }} color="black" mb={8}>
+          Improve your language skills through fun quizzes.
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default Mainpage;
